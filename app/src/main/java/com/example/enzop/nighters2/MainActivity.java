671569b731aeeff2556986866f21eaecfd29af8b
ClaraMorgane <enzo.piacenza@ensea.fr>
2016-11-20 18:21:29 -0600

    V.0.1 test

--- app/src/main/java/com/example/enzop/nighters2/MainActivity.java
+++ app/src/main/java/com/example/enzop/nighters2/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.enzop.nighters2;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -98,4 +99,10 @@ public class MainActivity extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+    public void result(View view){
+        Intent i = new Intent(getApplicationContext(), ResultActivity.class);
+        startActivity(i);
+
+    }
+
 }
